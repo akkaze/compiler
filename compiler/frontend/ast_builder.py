054efd22ed5c06d2e65d4e5781820b276557369a
zak-git <zhengankun@163.com>
2018-03-11 23:43:08 -0700

    add ast builder

--- compiler/frontend/ast_builder.py
+++ compiler/frontend/ast_builder.py
@@ -1,33 +1,40 @@
 from antlr4 import *
+from compiler.parser import MalicListener
+from compiler.frontend import AST
+from compiler.entity import *
+from compiler.type import *
+from compiler.ast import *
+from compiler.utils import InternalError
 
-class ASTBuilder(ParseTreeVisitor):
+class ASTBuilder(MalicListener):
     ast = None
-    self.map = dict()
-    def exitCompilationUnit(ctx):
+    map = dict()
+    def exitCompilationUnit(self, ctx):
         definition_nodes = []
         function_entities = []
         class_entities = []
         variable_entities = []
 
         for paser_rule_ctx in ctx.getTypedRuleContexts(ParserRuleContext):
-            definition_node = self.map[paser_rule_ctx]
+            node = self.map[paser_rule_ctx]
+            print(node.name)
             definition_nodes.append(node)
             if isinstance(node, FunctionDefNode):
-                function_entites.append(node)
-            elif isinstance(node, VaraibleDefNode):
+                function_entities.append(node)
+            elif isinstance(node, VariableDefNode):
                 variable_entities.append(node)
             elif isinstance(node, ClassDefNode):
                 class_entities.append(node)
             else:
                 raise InternalError('Invalid definition node ' + node.name)
 
-        ast = AST(definition_nodes, class_entites, 
-                functon_enties, variable_entities)
+        ast = AST(definition_nodes, class_entities, 
+                function_entities, variable_entities)
 
-    def exitClassDefinition(ctx):
+    def exitClassDefinition(self, ctx):
         vars = []
         funcs = []
-        name = ctx.name.getText()
+        name = ctx.name.text
 
         for item in ctx.variableDefinition():
             vars.append(self.map[item])
@@ -35,8 +42,8 @@ class ASTBuilder(ParseTreeVisitor):
         for item in ctx.functionDefinition():
             node = self.map[item]
             funcs.append(node)
-            entity = node.entity()
-            if entity.is_constructor():
+            entity = node.entity
+            if entity.is_constructor:
                 constuctor = entity
                 if entity.name != CONSTUCTOR_NAME + name:
                     raise SemanticError(Location(ctx.name), 
@@ -45,53 +52,50 @@ class ASTBuilder(ParseTreeVisitor):
                                         CONSTRUCTOR_NAME + name)
         entity = ClassEntity(Location(ctx.name), name, vars, funcs)
         entity.constructor = constructor
-        self.map[ctx] = entity
+        self.map[ctx] = ClassDefNode(entity)
 
-    def exitFunctionDefinition(ctx):
+    def exitFunctionDefinition(self, ctx):
         params = []
         for item in ctx.parameter():
             node = self.map[item]
             params.append(node)
         entity = None
-        if ctx.res == None:
-            entity = FunctionEntity(Location(ctx.name), ClassType(ctx.name.getText()),
+        if ctx.ret is None:
+            entity = FunctionEntity(Location(ctx.name), ClassType(ctx.name.text),
                                     CONSTURCTOR_NAME + ctx.name.getTxt(),
                                     params, self.map[ctx.block()])
             entity.contructor = True
         else:
             entity = FunctionEntity(Location(ctx.name), self.map[ctx.ret],
-                                    ctx.name.getTxt(),
+                                    ctx.name.text,
                                     params, self.map[ctx.block()])
-        
-    def exitVariableDefinition(ctx):
-        entity = VariableEntity(Location(ctx.Indentifier(), self.map[ctx.typeType()])),
-                                ctx.Identifier().getText(), get_expr(ctx.expression())
+        self.map[ctx] = FunctionDefNode(entity)
+
+    def exitVariableDefinition(self, ctx):
+        entity = VariableEntity(Location(ctx.Identifier()), self.map[ctx.typeType()], 
+                                ctx.Identifier().getText(), self.get_expr(ctx.expression()))
         self.map[ctx] = VariableDefNode(entity)
-    def exitParameter(ctx):
+    def exitParameter(self, ctx):
         self.map[ctx] = ParameterEntity(Location(ctx), self.map[ctx.typeType()],
-                                    ctx.Indentifier().getText())
-    def exitPrimitiveType(ctx):
-        stype = ctx.type.getText()
+                                    ctx.Indentifier().text)
+    def exitPrimitiveType(self, ctx):
+        stype = ctx.ttype.text
         if stype == 'bool':
             type = BoolType()
-            break
         elif stype == 'int':
             type = IntegerType()
-            break
         elif stype == 'void':
             type = VoidType()
-            break
         elif stype == 'string':
             type = StringType()
-            break
         else:
             raise InternalError('Invalid  type ' + stype)
         self.map[ctx] = type
 
-    def exitTypeType(ctx):
+    def exitTypeType(self, ctx):
         baseType = None
         if ctx.Identifier() != None:
-            base_type = ClassType(ctx.Indentifier.getText())
+            base_type = ClassType(ctx.Identifier().getText())
         else:
             base_type = self.map[ctx.primitiveType()]
         dimension = ctx.getChildCount() - 1 // 2
@@ -101,9 +105,9 @@ class ASTBuilder(ParseTreeVisitor):
             self.map[ctx] = ArrayType(base_type, dimension)
     def exitBlock(self, ctx):
         stmts = []
-        for item : ctx.statement():
-            stmt_node = self.get_stmt(item)
-            if stmt != None:
+        for item in ctx.statement():
+            stmt = self.get_stmt(item)
+            if stmt is not None:
                 stmts.append(stmt)
         self.map[ctx] = BlockNode(Location(ctx), stmts)
     def exitBlockStmt(self, ctx):
@@ -121,7 +125,7 @@ class ASTBuilder(ParseTreeVisitor):
     def exitWhileStmt(self, ctx):
         self.map[ctx] = WhileNode(Location(ctx), self.get_expr(ctx.expression()),
                             self.get_stmt(ctx.statement()))
-    def exitRetureStmt(self, ctx):
+    def exitReturnStmt(self, ctx):
         self.map[ctx] = ReturnNode(Location(ctx), self.get_expr(ctx.expression()))
     def exitBreakStmt(self, ctx):
         self.map[ctx] = BreakNode(Location(ctx))
@@ -134,13 +138,14 @@ class ASTBuilder(ParseTreeVisitor):
     def exitPrimaryExpr(self, ctx):
         self.map[ctx] = self.map[ctx.primary()]
     def exitMemberExpr(self, ctx):
-        self.map[ctx] = MemberNode(self.get_expr(ctx.expression()))
+        self.map[ctx] = MemberNode(self.get_expr(ctx.expression()),
+                                    ctx.Identifier().getText())
     def exitArefExpr(self, ctx):
         self.map[ctx] = ArefNode(self.get_expr(ctx.expression(0),
                                 self.get_expr(ctx.expression(1))))
-    def exitExprssionList(self, ctx):
+    def exitExpressionList(self, ctx):
         exprs = []
-        for x : ctx.expression():
+        for x in ctx.expression():
             exprs.append(self.get_expr(x))
         self.map[ctx] = exprs
 
@@ -157,29 +162,29 @@ class ASTBuilder(ParseTreeVisitor):
     
     def exitSuffixExpr(self, ctx):
         op = None
-        text = ctx.op.getText()
+        text = ctx.op.text
         if text == '++':
             op = UnaryOp.SUF_INC
         elif text == '--':
             op = UnaryOp.SUF_DEC
         else:
             raise InternalError('Invalid token ' + text)
-        self.map[ctx] = SuffixOpNode(op, self,get_expr(ctx.expression()))
+        self.map[ctx] = SuffixOpNode(op, self.get_expr(ctx.expression()))
 
     def exitPrefixNode(self, ctx):
         op = None
-        text = ctx.op.getText()
+        text = ctx.op.text
         if text == '+':
             op = UnaryOp.ADD
         elif text == '-':
             op = UnaryOp.MINUS
-        elif text == '++'
+        elif text == '++':
             op = UnaryOp.PRE_INC
-        elif text == '--'
+        elif text == '--':
             op = UnaryOp.PRE_DEC
-        elif text == '-'
+        elif text == '-':
             op = UnaryOp.BIT_NOT
-        elif text == '!'
+        elif text == '!':
             op = UnaryOp.LOGIC_NOT
         else:
             raise InternalError('Invalid token ' + text)
@@ -187,7 +192,7 @@ class ASTBuilder(ParseTreeVisitor):
 
     def exitBinaryExpr(self, ctx):
         op = None
-        text = ctx.op.getText()
+        text = ctx.op.text
         if text == '*':
             op = BinrayOp.MUL
         elif text == '/':
@@ -242,23 +247,23 @@ class ASTBuilder(ParseTreeVisitor):
         self.map[ctx] = VariableNode(Location(ctx.Identifier()),
                             ctx.Identifier().getText())
     def exitLiteralExpr(self, ctx):
-        self.map[ctx] = self.map[ctx.Literal()]]
-    def exitDecIntergerConst(self, ctx):
+        self.map[ctx] = self.map[ctx.literal()]
+    def exitDecIntegerConst(self, ctx):
         self.map[ctx] = IntegerLiteralNode(Location(ctx), 
             int(ctx.DecimalInteger().getText()))
     def exitStringConst(self, ctx):
-        value = ctx.StringLiteral().getText()
+        value = ctx.StringLiteral().text
         value = value[1:len(value) - 1]
         map[ctx] = StringLiteralNode(Location(ctx), value)
     def exitBoolConst(self, ctx):
         map[ctx] = BoolLiteralNode(Locaation(ctx), 
-            ctx.value.getText() == 'true')
+            ctx.value.text == 'true')
     def exitNullConst(self, ctx):
         map[ctx] = VariableNode(Location(ctx), 'null')
     def exitArrayCreator(self, ctx):
         base_type = None
         if ctx.Identifier() != None:
-            base_type = ClassType(ctx.Identifier().getText())
+            base_type = ClassType(ctx.Identifier().text)
         else:
             base_type = self.map[ctx.primitiveType()]
         exprs = ctx.expression()
@@ -271,7 +276,7 @@ class ASTBuilder(ParseTreeVisitor):
     def exitErrorCreator(self, ctx):
         raise SemanticError(Location(ctx), 'Invalid creator expression')
     def exitNonArrayCretor(self, ctx):
-        type = ClassType(ctx.Indentifier().getText())
+        type = ClassType(ctx.Indentifier().text)
         self.map[ctx] = CreatorNode(Location(ctx), type, None, 0)
     def get_stmt(self, ctx):
         if ctx == None:
